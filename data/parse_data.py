8ce0b146453bd021544ed59de3ee5348f3d6d778
Argha Chakrabarty <ac380012@gmail.com>
2021-10-24 04:59:01 +0530

    graph

--- data/parse_data.py
+++ data/parse_data.py
@@ -13,11 +13,11 @@ def get_file_contents(file_name):
 def get_aud_data(json):
     obj = {}
     key_val = [[x, y] for x, y in json.items()]
-    
+
     count_aud, count_vis = 0, 0
     for pair in key_val:
         # pprint(list(pair[1].values())[0]["session"])
-        
+
         if list(pair[1].values())[0]["session"] == "g0-train-aud":
             count_aud += 1
             obj[pair[0]] = pair[1]
@@ -34,7 +34,7 @@ def get_vis_data(json):
     count_aud, count_vis = 0, 0
     for pair in key_val:
         # pprint(list(pair[1].values())[0]["session"])
-        
+
         if list(pair[1].values())[0]["session"] == "g0-train-aud":
             count_aud += 1
         else:
@@ -49,19 +49,62 @@ def write_json(dict_obj, file_name):
         json.dump(dict_obj, out)
 
 
-def create_csv_aud_train(dict_obj):
+def create_csv_auth_gen(dict_obj):
+    session_names = ["g0-train-vis", "g0-auth-vis", "g0-auth-two-vis"]
 
-    session_names = ["g0-train-aud", "g0-auth-aud", "g0-auth-two-aud"]
-
-    with open("data_aud.csv", "w", newline="") as file:
+    with open("data_vis_auth.csv", "w", newline="") as file:
         csv_writer = csv.writer(file)
 
         rows = [
-                "user_id",
-                "pass_seq",
-                "hit_rate",
-            ]
-        
+            "user_id",
+            "pass_seq",
+            "hit_rate_train",
+            "hit_rate_auth",
+            "hit_rate_auth_pass",
+            "hit_rate_auth_two",
+            "hit_rate_auth_two_pass",
+        ]
+
+        csv_writer.writerow(rows)
+
+        for user in list(dict_obj.items()):
+            row = []
+            pass_seq = ""
+
+            for item in list(user[1].items()):
+                if item[0] == "passSeq":
+                    pass_seq = " ".join(item[1])
+
+                if item[0] not in ["noteGenerateLag", "noteSpeed", "passSeq"]:
+                    if item[1]["session"] == session_names[0]:
+                        row.append(item[1]["hitRate"])
+                    if item[1]["session"] == session_names[1]:
+                        row.append(item[1]["hitRate"])
+                        row.append(item[1]["block"][0]["passMetrics"]["hitRate"])
+                    if item[1]["session"] == session_names[2]:
+                        row.append(item[1]["hitRate"])
+                        row.append(item[1]["block"][0]["passMetrics"]["hitRate"])
+                        pass
+
+            row = [user[0], pass_seq, *row]
+            # pprint(row)
+            csv_writer.writerow(row)
+            pass
+
+
+def create_csv_aud_train(dict_obj):
+
+    session_names = ["g0-train-vis", "g0-auth-vis", "g0-auth-two-vis"]
+
+    with open("data_vis_train.csv", "w", newline="") as file:
+        csv_writer = csv.writer(file)
+
+        rows = [
+            "user_id",
+            "pass_seq",
+            "hit_rate",
+        ]
+
         for i in range(7):
             rows.append(f"block_{i + 1}_pass_hit_rate")
             rows.append(f"block_{i + 1}_noise_hit_rate")
@@ -71,30 +114,31 @@ def create_csv_aud_train(dict_obj):
         csv_writer.writerow(rows)
 
         for user in list(dict_obj.items()):
-            row = []        
+            row = []
             pass_seq = ""
 
             for item in list(user[1].items()):
                 if item[0] == "passSeq":
                     pass_seq = " ".join(item[1])
 
-                if (item[0] not in ["noteGenerateLag", "noteSpeed", "passSeq"]) and (item[1].get("session") == session_names[0]):
+                if (item[0] not in ["noteGenerateLag", "noteSpeed", "passSeq"]) and (
+                    item[1].get("session") == session_names[0]
+                ):
                     row.append(item[1].get("hitRate"))
                     for block in item[1].get("block"):
                         row.append(block.get("passMetrics").get("hitRate"))
                         row.append(block.get("noiseMetrics").get("hitRate"))
                         row.append(block.get("subBlockMetrics").get("hitRate"))
 
-
-            row = [user[0],pass_seq, *row]
+            row = [user[0], pass_seq, *row]
             csv_writer.writerow(row)
 
 
-
 if __name__ == "__main__":
-    json_data = get_file_contents("data_aud.json")
+    json_data = get_file_contents("data_vis.json")
 
     # write_json(get_aud_data(json_data), "data_aud.json")
     # write_json(get_vis_data(json_data), "data_vis.json")
 
     create_csv_aud_train(json_data)
+    # create_csv_auth_gen(json_data)
