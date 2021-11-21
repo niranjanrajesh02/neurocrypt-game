d42ded812087eaf3af53f74463196cde86924a2e
argha-dot <63609014+argha-dot@users.noreply.github.com>
2021-11-21 17:37:48 +0530

    change to auth two

--- src/ts/scenes/gameScene.ts
+++ src/ts/scenes/gameScene.ts
@@ -156,7 +156,7 @@ class GameScene extends Scene {
       hits: this.hits,
       misses: this.misses,
       hitRate: this.hitRate,
-      session: "g0-auth-vis",
+      session: "g0-auth-two-vis",
       block: [
         ...this.dataToSend.block,
         {
